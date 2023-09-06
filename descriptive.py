878ce5bc467b251bb7e33abea8e1455e347596f9
Vinicio Almeida <almeida.vinicio@gmail.com>
2023-09-06 11:04:51 -0300

    clean1

--- descriptive.py
+++ descriptive.py
@@ -1,22 +1,39 @@
 # Descriptive statistics
+import pandas as pd
+pd.set_option('display.float_format', lambda x: '%.2f' % x)
+import numpy as np
+import datetime
+import pandas_datareader.data as web   #pandas-datareader
+import yfinance as yf
+yf.pdr_override()
+import matplotlib.pyplot as plt
+from IPython.core.display import display, HTML   #ipython
+display(HTML("<style>.container { width:100% !important; }</style>"))
 
+# dates
+start = datetime.datetime(2000, 1, 2)
+end = datetime.datetime(2023, 9, 5)
+
+# fetch
 petr4 = web.get_data_yahoo('PETR4.SA',start,end) 
-petr4.head(3) # tres primeiros
-petr4.tail(3) # tres ultimos
-petr4.loc['2023-05-22'] # cotacao em dia especifico
-petr4.loc['2023-06-01':'2023-06-16'] # cotacoes em periodo especifico
 
-# grafico de preco
+# specific data
+petr4.head(3)
+petr4.tail(3)
+petr4.loc['2023-05-22'] 
+petr4.loc['2023-06-01':'2023-06-16'] 
+
+# price plot
 petr4['Adj Close'].plot()
-plt.xlabel("Data")
-plt.ylabel("Ajustado")
-plt.title("Preço de PETR4")
+plt.xlabel("Date")
+plt.ylabel("Adjusted")
+plt.title("PETR4 Price")
 plt.show()
 
 petr4_daily_returns = petr4['Adj Close'].pct_change()
 petr4_monthly_returns = petr4['Adj Close'].resample('M').ffill().pct_change()
 
-# grafico diario
+# daily returns plot
 fig = plt.figure()
 ax1 = fig.add_axes([0.1,0.1,0.8,0.8])
 ax1.plot(petr4_daily_returns)
@@ -25,7 +42,7 @@ ax1.set_ylabel("Percent")
 ax1.set_title("PETR4 daily returns data")
 plt.show()
 
-# grafico mensal
+# monthly returns plot
 fig = plt.figure()
 ax1 = fig.add_axes([0.1,0.1,0.8,0.8])
 ax1.plot(petr4_monthly_returns)
@@ -34,7 +51,7 @@ ax1.set_ylabel("Percent")
 ax1.set_title("PETR4 monthly returns data")
 plt.show()
 
-# histograma
+# histogram
 fig = plt.figure()
 ax1 = fig.add_axes([0.1,0.1,0.8,0.8])
 petr4_daily_returns.plot.hist(bins = 60)
@@ -44,12 +61,3 @@ ax1.set_title("PETR4 daily returns data")
 ax1.text(-0.35,200,"Extreme Low\nreturns")
 ax1.text(0.25,200,"Extreme High\nreturns")
 plt.show()
-
-
-multpl_stock_daily_returns = multpl_stocks['Adj Close'].pct_change()
-multpl_stock_monthly_returns = multpl_stocks['Adj Close'].resample('M').ffill().pct_change()
-
-fig =   plt.figure()
-(multpl_stock_monthly_returns + 1).cumprod().plot()
-plt.show()
-
