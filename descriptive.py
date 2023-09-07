4a7a3b7f30696707a430ef23522edc7e8ec8b59a
Vinicio Almeida <almeida.vinicio@gmail.com>
2023-09-07 07:16:04 -0300

    hist

--- descriptive.py
+++ descriptive.py
@@ -15,49 +15,49 @@ start = datetime.datetime(2000, 1, 2)
 end = datetime.datetime(2023, 9, 5)
 
 # fetch
-petr4 = web.get_data_yahoo('PETR4.SA',start,end) 
+bvsp = web.get_data_yahoo('^BVSP',start,end) 
 
-# specific data
-petr4.head(3)
-petr4.tail(3)
-petr4.loc['2023-05-22'] 
-petr4.loc['2023-06-01':'2023-06-16'] 
+# specific dates
+bvsp.head(3)
+bvsp.tail(3)
+bvsp.loc['2023-05-22'] 
+bvsp.loc['2023-06-01':'2023-06-16'] 
 
 # price plot
-petr4['Adj Close'].plot()
+bvsp['Adj Close'].plot()
 plt.xlabel("Date")
 plt.ylabel("Adjusted")
-plt.title("PETR4 Price")
+plt.title("Ibov Price")
 plt.show()
 
-petr4_daily_returns = petr4['Adj Close'].pct_change()
-petr4_monthly_returns = petr4['Adj Close'].resample('M').ffill().pct_change()
+bvsp_daily_returns = bvsp['Adj Close'].pct_change()
+bvsp_monthly_returns = bvsp['Adj Close'].resample('M').ffill().pct_change()
 
 # daily returns plot
 fig = plt.figure()
 ax1 = fig.add_axes([0.1,0.1,0.8,0.8])
-ax1.plot(petr4_daily_returns)
+ax1.plot(bvsp_daily_returns)
 ax1.set_xlabel("Date")
 ax1.set_ylabel("Percent")
-ax1.set_title("PETR4 daily returns data")
+ax1.set_title("Ibov daily returns data")
 plt.show()
 
 # monthly returns plot
 fig = plt.figure()
 ax1 = fig.add_axes([0.1,0.1,0.8,0.8])
-ax1.plot(petr4_monthly_returns)
+ax1.plot(bvsp_monthly_returns)
 ax1.set_xlabel("Date")
 ax1.set_ylabel("Percent")  
-ax1.set_title("PETR4 monthly returns data")
+ax1.set_title("Ibov monthly returns data")
 plt.show()
 
 # histogram
 fig = plt.figure()
 ax1 = fig.add_axes([0.1,0.1,0.8,0.8])
-petr4_daily_returns.plot.hist(bins = 60)
+bvsp_daily_returns.plot.hist(bins = 60)
 ax1.set_xlabel("Daily returns %")
 ax1.set_ylabel("Percent")
-ax1.set_title("PETR4 daily returns data")
+ax1.set_title("Ibov daily returns data")
 ax1.text(-0.35,200,"Extreme Low\nreturns")
 ax1.text(0.25,200,"Extreme High\nreturns")
 plt.show()
